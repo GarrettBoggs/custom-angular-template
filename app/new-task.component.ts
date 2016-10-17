b23853973a58049a2503f274440c09bcc8c19730
Garrett Boggs and Jason Downing <garrett+jason@>
2016-10-17 14:42:44 -0700

    create basic outline for to-do angular program

---
+++ app/new-task.component.ts
@@ -0,0 +1,24 @@
+import { Component, Output, EventEmitter } from "@angular/core";
+import { Task } from "./task.model";
+
+@Component ({
+  selector: "new-task",
+  template: `
+    <h1>Add a New Task</h1>
+    <div>
+      <label>Add a new Task description</label>
+      <input #taskDescription>
+      <label>Add a new Task number</label>
+      <input #taskId>
+      <button (click)="createButtonClicked(taskDescripton.value, taskId.value); taskDescription.value=''; taskId.value='';">Create Task</button>
+    </div>
+  `
+})
+
+export class NewTaskComponent {
+  @Output() newTaskSender = new EventEmitter()
+  createButtonClicked (description: string, id: number) {
+    var newTask: Task = new Task (description, id);
+    this.newTaskSender.emit(newTask);
+  }
+}
